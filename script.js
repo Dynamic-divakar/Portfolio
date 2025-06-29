b84be6f107627e1dd9e3d13dfaf63d86b35bb4e2
Divakar <160596227+Dynamic-divakar@users.noreply.github.com>
2025-06-29 22:59:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+document.querySelectorAll(".project-card").forEach((card) => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("flipped");
+  });
+});
